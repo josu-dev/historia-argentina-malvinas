f6446854630263d46002f255fc68e37bb2d93566
Josue <josue.sa.trabajos@gmail.com>
2025-07-04 15:13:04 -0300

    Fix outh callback url.

--- src/lib/server/auth/oauth.ts
+++ src/lib/server/auth/oauth.ts
@@ -1,7 +1,7 @@
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from "$env/static/private";
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, SITE_ORIGIN } from "$env/static/private";
 import { Google } from "arctic";
 
-export const google = new Google(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, "http://localhost:5173/login/google/callback");
+export const google = new Google(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, `${SITE_ORIGIN}/login/google/callback`);
 
 export const google_state_key = "google_oauth_state";
 
